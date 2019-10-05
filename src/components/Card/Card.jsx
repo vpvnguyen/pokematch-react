4daf45863692ad83a1e4a04fe4bd0efda6de4141
Vincent Nguyen <vpvnguyen@gmail.com>
2019-10-05 16:59:31 -0700

    renamed to .jsx

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+
+export default function Card() {
+    return (
+        <Container >
+            <p>Cards</p>
+            <ul>
+                <li>Get data from json</li>
+                <li>Randomize</li>
+                <li>Display to front</li>
+                <li>onclick: check if match</li>
+                <li>affect score</li>
+            </ul>
+        </Container>
+    )
+}
