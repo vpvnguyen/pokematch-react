7c2014e44b21010f6c6fa6795ec6274ec2a28c01
Vincent Nguyen <vpvnguyen@gmail.com>
2019-10-05 16:59:03 -0700

    import Header, Message, Card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+
+// styling
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+// components
+import Header from './components/Header/Header.jsx';
+import Message from './components/Message/Message.jsx';
+import Card from './components/Card/Card.jsx';
 
 function App() {
   return (
     <div className="App">
-      heyo world
+      <Header />
+      <Message />
+      <Card />
     </div>
   );
 }
