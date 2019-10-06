7996e3c4b95764dede0d4101d22e1148c46310a9
Vincent Nguyen <vpvnguyen@gmail.com>
2019-10-05 17:00:31 -0700

    add message body

---
+++ src/components/Message/Message.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
+export default function Message() {
+    return (
+        <Jumbotron >
+            <p>message</p>
+        </Jumbotron>
+    )
+}
