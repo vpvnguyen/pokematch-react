8d56bd1ce85e5e92ff22dd63d866988ffc9a81bf
Vincent Nguyen <vpvnguyen@gmail.com>
2019-10-05 19:09:18 -0700

    created api request model

---
+++ src/api/pokemon.api.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+// Export an object containing methods we'll use for accessing the Dog.Ceo API
+
+const getPokemon = {
+
+    pokemonApiRoute: 'https://api.pokemontcg.io/v1/cards',
+
+    getBasicCards: function () {
+        return axios.get(this.pokemonApiRoute)
+            .then(function (pokemonData) {
+                const pokemonCards = pokemonData.data.cards;
+                return getPokemon.shuffle(pokemonCards.filter(function (pokemonBasicResponse) { return pokemonBasicResponse.subtype === 'Basic' }));
+            })
+            .catch(function (err) {
+                if (err) throw err;
+            });
+    },
+
+    shuffle: function (array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    }
+}
+
+export default getPokemon;
