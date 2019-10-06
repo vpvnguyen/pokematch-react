358c50dff383068f8493515c0ecc728712db50dc
Vincent Nguyen <vpvnguyen@gmail.com>
2019-10-05 19:08:34 -0700

    removed Card; added Game

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import './App.css';
 // components
 import Header from './components/Header/Header.jsx';
 import Message from './components/Message/Message.jsx';
-import Card from './components/Card/Card.jsx';
+import Game from './components/Game/Game.jsx';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Message />
-      <Card />
+      <Game />
     </div>
   );
 }
