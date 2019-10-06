1a4dd61cebea1f857b2eff83acb6c77c09cc5478
Vincent Nguyen <vpvnguyen@gmail.com>
2019-10-05 23:51:33 -0700

    centered text

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 
-
 class Message extends Component {
 
     render() {
         return (
-            <Jumbotron >
+            <Jumbotron className="text-center">
                 <h3 className="mt-5">{this.props.message}</h3>
             </Jumbotron >
         )
