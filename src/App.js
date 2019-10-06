d6db5aa449aa0f66126d27dd6ac057df3c56b423
Vincent Nguyen <vpvnguyen@gmail.com>
2019-10-05 23:49:23 -0700

    removed bootstrap import

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
-// styling
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-
 // components
 import Game from './components/Game/Game.jsx';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Game />
     </div>
   );
