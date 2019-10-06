2ec94d77d6c232ad915d302710b184776d229154
Vincent Nguyen <vpvnguyen@gmail.com>
2019-10-05 22:15:19 -0700

    removed game components

--- src/App.js
+++ src/App.js
@@ -5,18 +5,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 // components
-import Header from './components/Header/Header.jsx';
-import Message from './components/Message/Message.jsx';
 import Game from './components/Game/Game.jsx';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Message />
       <Game />
     </div>
   );
 }
 
 export default App;
+
